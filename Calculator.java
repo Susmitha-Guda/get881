2871870372de90c69873f7eb3804d51e401e81dd
Susmitha-Guda <76148886+Susmitha-Guda@users.noreply.github.com>
2020-12-17 13:46:12 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,11 @@ class calculator{
 public static int add(int num1,int num2){
 return num1+num2;
 }
+  public static int sub(int num1,int num2){
+return num1-num2;
+}
 public static void main(String[] args){
 System.out.println(add(10,20));
+  System.out.println(sub(10,20));
 }
 }
