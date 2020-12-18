62ea2b570b1bb7d57594ad6ed306c1489e33a2ed
SUSMITHA <SUSMITHA@DESKTOP-O3JBL3C>
2020-12-18 15:48:55 +0530

    new

--- DemoApp/src/com/lti/model/calc.java
+++ DemoApp/src/com/lti/model/calc.java
@@ -11,5 +11,9 @@ public void sub(int num1,int num2)
 	System.out.println("Difference= "+(num1-num2));
 	System.out.println("changed one new");
 }
-
+public void prod(int num1,int num2)
+{
+	System.out.println("mul= "+(num1*num2));
+	
+}
 }
